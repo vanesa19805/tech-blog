52e4b44eaa68dd524c26053fc39a188c9d05a63c
vanesa gonzalez <gvane1919@gmail.com>
2021-04-05 02:19:53 -0400

    completed connections and user model to sign up

---
+++ models/user.js
@@ -0,0 +1,55 @@
+const { stubFalse } = require('lodash');
+const {model, DataTypes} = require('sequelize');
+const sequelize =require('../config/connection')
+
+class User extends Model {}
+
+User.init(
+    {
+id:{
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey:true,
+    autoIncrement: true,
+},
+username:{
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+},
+Passsword:{
+    type: DataTypes.STRING,
+    allowNull: false,
+    Validate:{
+        len: [8],
+    },
+},
+
+    },
+
+{
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
+          beforeUpdate: async (updatedUserData) => {
+            updatedUserData.password = await bcrypt.hash(
+              updatedUserData.password,
+              10
+            );
+            return updatedUserData;
+          },
+        },
+
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'User',
+},
+);
+
+module.exports = User; 
---
+++ public/sig-up.js
@@ -0,0 +1,9 @@
+async function newFormHandler(event){
+event.preventDefault();
+
+
+
+
+
+
+}
